a5b2fb2c13e6ea7d87daa2e8b682d6ef2689c318
chenyijie <chenyijie@cd.tuan800.com>
2017-09-06 18:48:33 +0800

    init

---
+++ src/main/java/telemarketer/skittlealley/SkittleAlleyApplication.java
@@ -0,0 +1,12 @@
+package telemarketer.skittlealley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkittleAlleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkittleAlleyApplication.class, args);
+	}
+}
