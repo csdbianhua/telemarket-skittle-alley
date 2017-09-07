4b3a87726327f9d873343cfd2c542426e979887b
chenyijie <chenyijie@cd.tuan800.com>
2017-09-07 13:23:25 +0800

    修改启动脚本

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -4,7 +4,7 @@ if [ -n "`lsof -i:9607`" ]; then
     echo 已有服务占用9607端口,服务可能已启动
     exit
 fi
-jar_name=`ls -t skittle-alley-*.jar | head -1`
+jar_name=skittle-alley.jar
 if [ -z "${jar_name}" ]; then
     echo 不存在服务主文件,请检查文件完整性
     exit
